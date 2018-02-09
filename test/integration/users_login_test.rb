12cdee717856d4f84bf9101573dda414dedb17ce
Victor <vicsmaloba@gmail.com>
2018-02-09 11:08:44 +0300

    Finish user edit, update, index, and destroy actions

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -56,6 +56,6 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 				# Log in again and verify that the cookie is deleted.
 				log_in_as(@user, remember_me: '0')
 				assert_empty cookies['remember_token']
-
 		end
+
 end
